58c32b648ef021eb42707f63d3d7ee42fada8185
Pavel Liolia <pliolia@suvoda.com>
2025-10-03 13:36:26 +0300

    feat: add main page, avatar, posts page

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'Посты',
+  description: 'Список постов.',
+}
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Посты</h1>
+      TBD
+    </section>
+  )
+}
