5a129dfb1bdcbf8fa5ff50ee181684c169c1df3e
Pavel Liolia <pliolia@suvoda.com>
2025-10-05 13:53:32 +0300

    fix: unoptimize images

--- src/components/RoundedImage.tsx
+++ src/components/RoundedImage.tsx
@@ -2,5 +2,7 @@ import Image from "next/image";
 import React from "react";
 
 export function RoundedImage(props: any) {
-  return <Image alt={props.alt} className="rounded-lg" {...props} />;
+  return (
+    <Image alt={props.alt} className="rounded-lg" unoptimized {...props} />
+  );
 }
