f0454996d16bedadf3df5ce914d11e28724d0f87
Pavel Liolia <pliolia@suvoda.com>
2025-10-05 12:59:09 +0300

    fix: remouve double-slash in images urls

--- public-loader.ts
+++ public-loader.ts
@@ -1,4 +1,4 @@
 export default function publicLoader({ src }: { src: string }) {
   const basePath = process.env.BASE_PATH || "";
-  return basePath ? `${basePath}/${src}` : src;
+  return basePath ? `${basePath}${src}` : src;
 }
