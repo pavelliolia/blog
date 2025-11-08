b3fdfa3bbad4a22b922eeef77dbc783ff706a6c3
Pavel Liolia <pliolia@suvoda.com>
2025-11-08 15:54:05 +0300

    feat: add cyrillic anchors translit & update styles

--- src/utils/createHeading.ts
+++ src/utils/createHeading.ts
@@ -1,19 +1,40 @@
+import CyrillicToTranslit from "cyrillic-to-translit-js";
 import React from "react";
 
-function slugify(str: any) {
-  return str
-    .toString()
+const cyrillicToTranslit = CyrillicToTranslit();
+
+function slugify(heading: string) {
+  return cyrillicToTranslit
+    .transform(heading, "-")
     .toLowerCase()
     .trim() // Remove whitespace from both ends of a string
-    .replace(/\s+/g, "-") // Replace spaces with -
     .replace(/&/g, "-and-") // Replace & with 'and'
     .replace(/[^\w\-]+/g, "") // Remove all non-word characters except for -
     .replace(/\-\-+/g, "-"); // Replace multiple - with single -
 }
 
+function extractStringHeading(children: React.ReactNode): string {
+  return (
+    React.Children.map(children, (child) => {
+      if (typeof child === "object") {
+        const nestedChildren = (
+          child as React.ReactElement<{ children: React.ReactNode }>
+        ).props.children;
+        return extractStringHeading(nestedChildren);
+      } else if (typeof child === "string") {
+        return child;
+      } else {
+        return child?.toString();
+      }
+    })?.join("-") ?? ""
+  );
+}
+
 export function createHeading(level: number) {
   const Heading = ({ children }: { children: React.ReactNode }) => {
-    const slug = slugify(children);
+    const stringHeading = extractStringHeading(children);
+    const slug = slugify(stringHeading);
+
     return React.createElement(
       `h${level}`,
       { id: slug },
